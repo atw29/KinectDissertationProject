d91aeac27f012b4fe25d8e3c24969ee60963297e
Alex Weeks <atw29@bath.ac.uk>
2019-02-21 14:05:23 +0000

    Added Logic to Lighting Control. Implemented 2D manipulation

--- KinectDissertationProject/Views/Tasks/LightingControl.xaml.cs
+++ KinectDissertationProject/Views/Tasks/LightingControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using KinectDissertationProject.ViewModel;
+﻿using KinectDissertationProject.Models;
+using KinectDissertationProject.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,49 @@ namespace KinectDissertationProject.Views.Tasks
             ViewModel = KinectViewModel.Instance;
 
             InitializeComponent();
+
+            ViewModel.GestureOccurred += ViewModel_GestureOccurred;
+        }
+
+        private void ViewModel_GestureOccurred(object sender, WindowOperationEventArgs e)
+        {
+            switch (e.Gesture)
+            {
+                case GestureType.RIGHT_SWIPE_LEFT_LEFT_HAND_RAISED:
+                    DecreaseSlider();
+                    break;
+                case GestureType.RIGHT_SWIPE_RIGHT_LEFT_HAND_RAISED:
+                    IncreaseSlider();
+                    break;
+                case GestureType.RIGHT_HAND_SWIPE_DOWN:
+                    ChangeSelection();
+                    break;
+                case GestureType.RIGHT_HAND_SWIPE_UP:
+                    ChangeSelection();
+                    break;
+                    
+            }
+        }
+
+        private void DecreaseSlider()
+        {
+            slider.Value = slider.Value - 5 % 100;
+        }
+
+        private void IncreaseSlider()
+        {
+            slider.Value = slider.Value + 5 % 100;
+        }
+
+        private void ChangeSelection()
+        {
+            if (Exit.IsFocused)
+            {
+                Apply.Focus();
+            } else
+            {
+                Exit.Focus();
+            }
         }
 
         internal KinectViewModel ViewModel { get; }
